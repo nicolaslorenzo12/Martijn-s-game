c2baaead6b6f2c7771bf3ac86d5314d7e5eb3b8c
Nicolas Lorenzo <nicolaslorenzohermosilla@gmail.com>
2022-02-02 12:00:36 +0100

    Just starting to move around

--- src/Room.java
+++ src/Room.java
@@ -3,14 +3,60 @@ import java.util.Scanner;
 public class Room {
 
     Scanner scanner;
+     private String roomName;
 
-    public Room(){
+    public Room(String roomName){
+        this.roomName = roomName;
         scanner = new Scanner(System.in);
     }
-    
-    Room room1 = new Room();
-    Room room2 = new Room();
-    Room room3 = new Room();
-    Room room4 = new Room();
-    Room room5 = new Room();
+
+    Room lobby = new Room("Lobby");
+    Room upRoom = new Room("Nico'sRoom");
+    Room downRoom = new Room("Martijn'sRoom");
+    Room rightRoom = new Room("Gaby'sRoom");
+    Room leftRoom = new Room("Pat'sRoom");
+
+    public void welcomeToLobby(){
+        System.out.println("Welcome to the lobby");
+        boolean correctDirection = false;
+
+        while(!correctDirection) {
+            System.out.println("Please enter 'up' to go nico's room, 'down' to go to martijn's room, 'right' to go to gaby's room and 'left' to go to pat's room ");
+            String answer = scanner.nextLine();
+            if (answer.equals("up")) {
+                correctDirection = true;
+                welcomeToNicoRoom();
+            } else if (answer.equals("down")) {
+                correctDirection = true;
+                welcomeToMartijnRoom();
+            }
+            else if(answer.equals("right")){
+                correctDirection = true;
+                welcomeToGabyRoom();
+            }
+            else if(answer.equals("left")){
+                correctDirection = true;
+                welcomeToPatRoom();
+            }
+            else{
+                System.out.println("Not a valid direction");
+            }
+        }
+    }
+
+    public void welcomeToNicoRoom(){
+
+    }
+
+    public void welcomeToMartijnRoom(){
+
+    }
+
+    public void welcomeToGabyRoom(){
+
+    }
+
+    public void welcomeToPatRoom(){
+
+    }
 }
