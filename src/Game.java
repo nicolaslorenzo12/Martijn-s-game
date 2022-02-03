32ff28fb9f5bd9dc64947dccb0969155f46f0397
Nicolas Lorenzo <nicolaslorenzohermosilla@gmail.com>
2022-02-03 21:47:52 +0100

    creating the game, please do not scold me if there are flaws. I am already aware of many of them.

---
+++ src/Game.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Game {
+
+    public static void main(String[] args) {
+        Room room = new Room();
+        room.welcomeToLobby();
+    }
+}
--- src/Room.java
+++ src/Room.java
@@ -2,61 +2,119 @@ import java.util.Scanner;
 
 public class Room {
 
-    Scanner scanner;
-     private String roomName;
+    private final Scanner scanner;
+    private String direction;
 
-    public Room(String roomName){
-        this.roomName = roomName;
+    public Room(){
+        direction = "whatever";
         scanner = new Scanner(System.in);
     }
 
-    Room lobby = new Room("Lobby");
-    Room upRoom = new Room("Nico'sRoom");
-    Room downRoom = new Room("Martijn'sRoom");
-    Room rightRoom = new Room("Gaby'sRoom");
-    Room leftRoom = new Room("Pat'sRoom");
-
     public void welcomeToLobby(){
-        System.out.println("Welcome to the lobby");
-        boolean correctDirection = false;
 
-        while(!correctDirection) {
-            System.out.println("Please enter 'up' to go nico's room, 'down' to go to martijn's room, 'right' to go to gaby's room and 'left' to go to pat's room ");
-            String answer = scanner.nextLine();
-            if (answer.equals("up")) {
-                correctDirection = true;
-                welcomeToNicoRoom();
-            } else if (answer.equals("down")) {
-                correctDirection = true;
-                welcomeToMartijnRoom();
-            }
-            else if(answer.equals("right")){
-                correctDirection = true;
-                welcomeToGabyRoom();
-            }
-            else if(answer.equals("left")){
-                correctDirection = true;
-                welcomeToPatRoom();
-            }
-            else{
-                System.out.println("Not a valid direction");
-            }
+        System.out.println("Welcome to the lobby");
+        direction = "whatever";
+
+        while(!direction.equals("up") && !direction.equals("down") && !direction.equals("right") && !direction.equals("left")){
+            System.out.print("Please type 'up' to go to nico's room, 'down' to go to martijn's room, 'right' to go to gaby's room, 'left' to go to pat's room. Be careful when you type: ");
+            direction = scanner.nextLine();
+        }
+
+        switch (direction) {
+            case "up" -> welcomeToNicoRoom();
+            case "down" -> welcomeToMartijnRoom();
+            case "right" -> welcomeToGabyRoom();
+            default -> welcomeToPatRoom();
         }
     }
 
     public void welcomeToNicoRoom(){
 
+        System.out.println("Welcome to nico's room");
+        
+        while(!direction.equals("down")){
+            System.out.print("Please type 'down' to go to the lobby. Be careful when you type: ");
+            direction = scanner.nextLine();
+        }
+        welcomeToLobby();
+
     }
 
     public void welcomeToMartijnRoom(){
+
+        System.out.println("Welcome to martijn's room");
+        direction = "whatever";
+
+        while(!direction.equals("up") && !direction.equals("down") && !direction.equals("right") && !direction.equals("left")){
+            System.out.print("Please type 'up' to go to the lobby,'down' to go to gert's room, 'right' to go to marku's room,'left' to go to stijn's room. Be careful when you type: ");
+            direction = scanner.nextLine();
+        }
+
+        switch (direction) {
+            case "up" -> welcomeToLobby();
+            case "down" -> welcomeToGertRoom();
+            case "right" -> welcomeToMarkusRoom();
+            default -> welcomeToStijnRoom();
+        }
 
     }
 
     public void welcomeToGabyRoom(){
 
+        System.out.println("Welcome to gaby's room");
+
+        while(!direction.equals("left")){
+            System.out.print("Please type 'left' to go to the lobby. Be careful when you type: ");
+            direction = scanner.nextLine();
+        }
+        welcomeToLobby();
+
     }
 
     public void welcomeToPatRoom(){
 
+        System.out.println("Welcome to pat's room");
+
+        while(!direction.equals("right")){
+            System.out.print("Please type 'right' to go to the lobby. Be careful when you type: ");
+            direction = scanner.nextLine();
+        }
+        welcomeToLobby();
+
     }
+
+    public void welcomeToMarkusRoom(){
+
+        System.out.println("Welcome to markus's room");
+
+        while(!direction.equals("left")){
+            System.out.print("Please type 'left' to go to martijn's room. Be careful when you type: ");
+            direction = scanner.nextLine();
+        }
+        welcomeToMartijnRoom();
+    }
+
+    public void welcomeToStijnRoom(){
+
+        System.out.println("Welcome to stijn's room");
+
+        while(!direction.equals("right")){
+            System.out.print("Please type 'right' to go to martijn's room. Be careful when you type: ");
+            direction = scanner.nextLine();
+        }
+        welcomeToMartijnRoom();
+    }
+
+    public void welcomeToGertRoom(){
+
+        System.out.println("Welcome to gert's room");
+
+        while(!direction.equals("up")){
+            System.out.print("Please type 'up' to go to martijn's room. Be careful when you type: ");
+            direction = scanner.nextLine();
+        }
+        welcomeToMartijnRoom();
+    }
+
+
 }
