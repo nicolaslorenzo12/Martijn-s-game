68a1aad2743f7fdc13ec2867132998e634ff17dd
Nicolas Lorenzo <nicolaslorenzohermosilla@gmail.com>
2022-02-09 14:27:12 +0100

    adding setter to put the different exits of a room, there is an error

--- src/Game.java
+++ src/Game.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class Game {
 
     public static void main(String[] args) {
+        Room room = new Room();
+        System.out.println(room.getRooms());
     }
 }
--- src/Room.java
+++ src/Room.java
@@ -6,34 +6,56 @@ public class Room {
     private final Scanner scanner;
     private String direction;
     private final ArrayList<Room> rooms;
+    private Room upRoom;
+    private Room downRoom;
+    private Room leftRoom;
+    private Room rightRoom;
+    private String name;
 
-    public Room(String upRoom, String downRoom, String leftRoom, String rightRoom) {
+    public Room() {
         direction = "whatever";
         scanner = new Scanner(System.in);
         rooms = new ArrayList<>();
-        Room lobby = new Room("nicoRoom", "martijnRoom", "patRoom", "gabyRoom");
+        this.name = name;
+        lobby.setRooms(nicoRoom,martijnRoom,gabyRoom,patRoom,"Lobby");
+        nicoRoom.setRooms(null,lobby,null,null,"Nico's Room");
+        martijnRoom.setRooms(lobby,markusRoom,gertRoom,stijnRoom,"Martijn's Room");
+        gabyRoom.setRooms(null,null,null,lobby,"Gaby's Room");
+        patRoom.setRooms(null,null,lobby,null,"Pat's Room");
+        markusRoom.setRooms(martijnRoom,null,null,null,"Markus's Room");
+        gertRoom.setRooms(null,null,null,martijnRoom,"Gert's Room");
+        stijnRoom.setRooms(null,null,martijnRoom,null,"Stijn's Room");
         rooms.add(lobby);
-        Room nicoRoom = new Room(null, "lobby", null, null);
         rooms.add(nicoRoom);
-        Room martijnRoom = new Room("lobby", "markusRoom", "gertRoom", "stijnRoom");
         rooms.add(martijnRoom);
-        Room patRoom = new Room(null, null, null, "lobby");
-        rooms.add(patRoom);
-        Room gabyRoom = new Room(null, null, "lobby", null);
         rooms.add(gabyRoom);
-        Room markusRoom = new Room("martijnRoom", null, null, null);
+        rooms.add(patRoom);
         rooms.add(markusRoom);
-        Room stijnRoom = new Room(null, null, "martijnRoom", null);
-        rooms.add(stijnRoom);
-        Room gertRoom = new Room(null, null, null, "martijnRoom");
         rooms.add(gertRoom);
+        rooms.add(stijnRoom);
     }
 
+    Room lobby = new Room();
+    Room nicoRoom = new Room();
+    Room martijnRoom = new Room();
+    Room gabyRoom = new Room();
+    Room patRoom = new Room();
+    Room markusRoom = new Room();
+    Room gertRoom = new Room();
+    Room stijnRoom = new Room();
 
-    public void movingToOtherRoom(String room, String direction){
-        System.out.println("Currently you are in ");
-        System.out.println("Please enter a direction, 'up', 'down', 'right, 'left' to move to another room. However, you will only move if it is possible. Otherwise, enter a new direction ");
+    public void setRooms(Room upRoom, Room downRoom, Room rightRoom, Room leftRoom, String name){
+        this.upRoom = upRoom;
+        this.downRoom = downRoom;
+        this.rightRoom = rightRoom;
+        this.leftRoom = leftRoom;
+        this.name = name;
     }
+
+    public ArrayList<Room> getRooms(){
+        return rooms;
+    }
+
 
 }
 
