a5cb20cd763204f41a60641653b509004e2074b3
Nicolas Lorenzo <nicolaslorenzohermosilla@gmail.com>
2022-02-09 22:19:38 +0100

    Connecting the rooms

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,35 @@
-import java.util.Scanner;
-
 public class Game {
 
+    public Game() {
+
+        Room lobby = new Room();
+        Room martijnRoom = new Room();
+        Room nicoRoom = new Room();
+        Room patRoom = new Room();
+        Room gabyRoom = new Room();
+        Room gertRoom = new Room();
+        Room stijnRoom = new Room();
+        Room markusRoom = new Room();
+
+        lobby.setName("lobby");
+        lobby.setUpRoom(nicoRoom);
+        lobby.setDownRoom(martijnRoom);
+        lobby.setRightRoom(gabyRoom);
+        lobby.setLeftRoom(patRoom);
+        nicoRoom.setDownRoom(lobby);
+        gabyRoom.setLeftRoom(lobby);
+        patRoom.setRightRoom(lobby);
+        martijnRoom.setUpRoom(lobby);
+        martijnRoom.setDownRoom(markusRoom);
+        martijnRoom.setRightRoom(gertRoom);
+        martijnRoom.setLeftRoom(stijnRoom);
+        markusRoom.setUpRoom(martijnRoom);
+        gertRoom.setLeftRoom(martijnRoom);
+        stijnRoom.setRightRoom(martijnRoom);
+    }
+
     public static void main(String[] args) {
-        Room room = new Room();
-        System.out.println(room.getRooms());
+
+
     }
 }
--- src/Room.java
+++ src/Room.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 public class Room {
 
     private final Scanner scanner;
-    private String direction;
-    private final ArrayList<Room> rooms;
     private Room upRoom;
     private Room downRoom;
     private Room leftRoom;
@@ -13,49 +11,49 @@ public class Room {
     private String name;
 
     public Room() {
-        direction = "whatever";
         scanner = new Scanner(System.in);
-        rooms = new ArrayList<>();
-        this.name = name;
-        lobby.setRooms(nicoRoom,martijnRoom,gabyRoom,patRoom,"Lobby");
-        nicoRoom.setRooms(null,lobby,null,null,"Nico's Room");
-        martijnRoom.setRooms(lobby,markusRoom,gertRoom,stijnRoom,"Martijn's Room");
-        gabyRoom.setRooms(null,null,null,lobby,"Gaby's Room");
-        patRoom.setRooms(null,null,lobby,null,"Pat's Room");
-        markusRoom.setRooms(martijnRoom,null,null,null,"Markus's Room");
-        gertRoom.setRooms(null,null,null,martijnRoom,"Gert's Room");
-        stijnRoom.setRooms(null,null,martijnRoom,null,"Stijn's Room");
-        rooms.add(lobby);
-        rooms.add(nicoRoom);
-        rooms.add(martijnRoom);
-        rooms.add(gabyRoom);
-        rooms.add(patRoom);
-        rooms.add(markusRoom);
-        rooms.add(gertRoom);
-        rooms.add(stijnRoom);
     }
 
-    Room lobby = new Room();
-    Room nicoRoom = new Room();
-    Room martijnRoom = new Room();
-    Room gabyRoom = new Room();
-    Room patRoom = new Room();
-    Room markusRoom = new Room();
-    Room gertRoom = new Room();
-    Room stijnRoom = new Room();
 
-    public void setRooms(Room upRoom, Room downRoom, Room rightRoom, Room leftRoom, String name){
+    public Room getUpRoom() {
+        return upRoom;
+    }
+
+    public void setUpRoom(Room upRoom) {
         this.upRoom = upRoom;
+    }
+
+    public Room getDownRoom() {
+        return downRoom;
+    }
+
+    public void setDownRoom(Room downRoom) {
         this.downRoom = downRoom;
-        this.rightRoom = rightRoom;
+    }
+
+    public Room getLeftRoom() {
+        return leftRoom;
+    }
+
+    public void setLeftRoom(Room leftRoom) {
         this.leftRoom = leftRoom;
+    }
+
+    public Room getRightRoom() {
+        return rightRoom;
+    }
+
+    public void setRightRoom(Room rightRoom) {
+        this.rightRoom = rightRoom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
-
-    public ArrayList<Room> getRooms(){
-        return rooms;
-    }
-
 
 }
 
